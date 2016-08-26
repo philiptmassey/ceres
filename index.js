886a5bd34afbfc05035592a1cee8a527d53ccc8e
philiptmassey <philip.t.massey@gmail.com>
2016-08-26 14:34:02 -0700

    Added a new row into the database

--- index.js
+++ index.js
@@ -4,8 +4,36 @@ var pg = require('pg');
 var app = express();
 app.set('port', (process.env.PORT || 8080));
 
+// bodyParse.urlencoded(options)
+// Parses the text as URL encoded data and exposes the resulting object on
+// req.body
+// from http://stackoverflow.com/questions/4295782/how-do-you-extract-post-data-in-node-js
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.get('/', function(request, response) {
     response.send('Hello Ken!');
+});
+
+app.post('/add', function(request, response) {
+    id = req.body.id
+    data = req.body.data
+
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            response.status(500).send('Error connecting to database.');
+            return;
+        }
+        client.query('INSERT into data (id, data) values($1, $2)',
+                     [id, data], function(err, result) {
+            done();
+            if (error) {
+                response.status(500).send('Error inserting into database.');
+                return;
+            }
+         });
+    });
 });
 
 app.get('/datadump', function(request, response) {
@@ -15,9 +43,10 @@ app.get('/datadump', function(request, response) {
             return;
         }
         client.query('SELECT * from data', function(err, result) {
+            done();
             if (err) {
                 response.status(500).send('Error selecting in database.');
-                return
+                return;
             } else {
                 response.setHeader('Content-Type', 'application/json');
                 response.send(JSON.stringify(result.rows));
