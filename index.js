53436c08eff83179b088f9097aab8d70b29ddaca
philiptmassey <philip.t.massey@gmail.com>
2016-08-25 20:41:45 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8080));
+
+app.get('/', function(request, response) {
+    response.write('Hello Ken!');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
