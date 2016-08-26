c2e0b279d5c917bcf936167e012fa803d5775f4c
philiptmassey <philip.t.massey@gmail.com>
2016-08-26 14:17:33 -0700

    Added JSON data response

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var express = require('express');
-var app = express();
+var pg = require('pg');
 
+var app = express();
 app.set('port', (process.env.PORT || 8080));
 
 app.get('/', function(request, response) {
     response.send('Hello Ken!');
+});
+
+app.get('/datadump', function(request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            response.status(500).send('Error connecting to database.');
+            return;
+        }
+        client.query('SELECT * from data', function(err, result) {
+            if (err) {
+                response.status(500).send('Error selecting in database.');
+                return
+            } else {
+                response.setHeader('Content-Type', 'application/json');
+                response.send(JSON.stringify(result.rows));
+            }
+        });
+    });
 });
 
 app.listen(app.get('port'), function() {
